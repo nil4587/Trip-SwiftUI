70e1bf9ba55365fa1213b16418f3ce417eba0a4c
NileshKumar Prajapati <3828906+nil4587@users.noreply.github.com>
2023-04-22 15:56:56 +0200

    Initial Commit

---
+++ Trip/TripApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripApp.swift
+//  Trip
+//
+//  Created by Nileshkumar M. Prajapati on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
